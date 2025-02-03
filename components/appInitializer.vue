59a0958a3a23a922d2cf94d10a18d3a5d043d3fc
TsionTesfaye <tsitesfaye87466@gmail.com>
2025-02-03 23:43:55 +0300

    semi colon removed

--- components/appInitializer.vue
+++ components/appInitializer.vue
@@ -1,16 +1,16 @@
 <!-- ~/components/AppInitializer.vue -->
 <script setup lang="ts">
-import { useSession } from "~/composables/useSession";
-import { useRouter } from "vue-router";
+import { useSession } from "~/composables/useSession" 
+import { useRouter } from "vue-router" 
 
-const { session } = useSession();
-const router = useRouter();
+const { session } = useSession() 
+const router = useRouter() 
 
 // Redirect based on session
 if (!session.value) {
-  router.push("/"); // No session, redirect to login
+  router.push("/")  // No session, redirect to login
 } else {
-  router.push("/todo"); // Session exists, redirect to todo
+  router.push("/todo")  // Session exists, redirect to todo
 }
 </script>
 
